488683d1719470e00370421617b5d2c59c071b0a
kds <koutsenko@gmail.com>
2020-04-09 15:39:41 +0300

    Added formik, ported code

--- src/components/FormikDemo/index.js
+++ src/components/FormikDemo/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { Formik, Field, Form } from "formik";
 
 import Debug from "../Debug";
 
@@ -6,62 +7,28 @@ const defaultDelay = 1000;
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const useFormik = (props) => {
-  const [values, setValues] = useState(props.initialValues || {});
-
-  const handleChange = (name) => (event) => {
-    event.preventDefault();
-    event.persist();
-
-    setValues((oldValues) => ({
-      ...oldValues,
-      [name]: event.target.value,
-    }));
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    await props.onSubmit(values);
-  };
-
-  return { values, handleChange, handleSubmit };
-};
-
-const FormikDemo = () => {
-  const { values, handleChange, handleSubmit } = useFormik({
-    initialValues: {
-      login: "",
-      password: "",
-    },
-    onSubmit: async () => {
-      await delay(defaultDelay);
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-
-  return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <label>Логин</label>
-        <input
-          type="text"
-          value={values.login}
-          onChange={handleChange("login")}
-        />
-        <br />
-        <label>Пароль</label>
-        <input
-          type="text"
-          value={values.password}
-          onChange={handleChange("password")}
-        />
-        <br />
-        <input type="submit" value="Войти" />
-      </form>
-      <Debug {...values} />
-    </>
-  );
-};
+const FormikDemo = () => (
+  <>
+    <Formik
+      initialValues={{ login: "", password: "" }}
+      onSubmit={async (values) => {
+        await delay(defaultDelay);
+        alert(JSON.stringify(values, null, 2));
+      }}
+      render={({ values }) => (
+        <Form>
+          <label htmlFor="login">Логин</label>
+          <Field name="login" placeholder="Login" />
+          <br />
+          <label htmlFor="password">Пароль</label>
+          <Field name="password" placeholder="Password" />
+          <br />
+          <input type="submit" value="Войти" />
+          <Debug {...values} />
+        </Form>
+      )}
+    />
+  </>
+);
 
 export default FormikDemo;
