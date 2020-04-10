9087670c1e30ce06c2ad6f604535019aec80149c
kds <koutsenko@gmail.com>
2020-04-10 14:31:02 +0300

    Updates

---
+++ precache-manifest.26f061e84a390c4341a9f50632f8e787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3325ee9226820d43b1db6ae337458f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28c67f04840f794063f9",
+    "url": "/static/css/main.767a7547.chunk.css"
+  },
+  {
+    "revision": "b74d04c4abcd0528ddd2",
+    "url": "/static/js/2.de8ec206.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.de8ec206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28c67f04840f794063f9",
+    "url": "/static/js/main.e4b6ae90.chunk.js"
+  },
+  {
+    "revision": "916ecc2862f4030ac7a6",
+    "url": "/static/js/runtime-main.66bee823.js"
+  }
+]);
