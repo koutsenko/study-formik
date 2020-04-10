2579c9cc89ddc30a3e2ffb716018ba5ff54723da
kds <koutsenko@gmail.com>
2020-04-10 12:36:50 +0300

    Imitate server errors

--- src/components/FormikDemo/index.js
+++ src/components/FormikDemo/index.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Formik, Field, Form, useField, ErrorMessage } from "formik";
+import { Formik, Form, useField, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 import Debug from "../Debug";
 
 const defaultDelay = 1000;
+
+const imitateRequest = async (data) => {
+  await delay(defaultDelay);
+
+  const errors = [];
+
+  if (data.login !== "demo@mail.ru") {
+    errors.push({ login: "Пользователь не существует" });
+  }
+
+  if (data.login === "demo@mail.ru" && data.password !== "12345678") {
+    errors.push({ password: "Пароль неверный" });
+  }
+
+  return errors;
+};
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -34,9 +50,18 @@ const FormikDemo = () => {
   /**
    * Read docs https://jaredpalmer.com/formik/docs/guides/form-submission
    */
-  const submit = async (values) => {
-    await delay(defaultDelay);
-    alert(JSON.stringify(values, null, 2));
+  const submit = async (values, bag) => {
+    const errors = await imitateRequest(values);
+    if (errors.length === 0) {
+      alert("Вход успешный, можно редиректить");
+    } else {
+      const collectedErrors = errors.reduce(
+        (acc, cur) => ({ ...acc, ...cur }),
+        {}
+      );
+
+      bag.setErrors(collectedErrors);
+    }
   };
 
   return (
